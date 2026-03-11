1e6d4c44efd2f45afc60d1ff0997d28e07b85ace
NguyenHongPhong <phongnguyenwn123@gmail.com>
2026-03-12 06:32:04 +0700

    feat: code some detail in QA page

--- app/[locale]/qa/page.tsx
+++ app/[locale]/qa/page.tsx
@@ -3,6 +3,7 @@ import { getDictionary } from "@/lib/get-dictionary";
 import Image from "next/image";
 import StackIcon from "tech-stack-icons";
 import { IoMdCheckmark } from "react-icons/io";
+import { FaPlay } from "react-icons/fa";
 export default async function QA(props: paramProps) {
     const localeParam = await props.params;
     const { locale } = localeParam;
@@ -12,44 +13,50 @@ export default async function QA(props: paramProps) {
         <div className="@container">
             <h1 className="text-black dark:text-white font-bold text-2xl mb-3 text-center">{dict.QA.heading}</h1>
             <h2 className="text-black dark:text-white font-bold text-xl mb-3 text-center">{dict.QA.subheading}</h2>
-            <div className="flex flex-col md:flex-row gap-4">
-                <div>
-                    <Image
-                        src="/portfolio-img-2.jpg"
-                        alt="portfolio-img-2"
-                        width={0}
-                        height={0}
-                        className="w-full xl:w-3/5 h-72 object-cover object-bottom rounded-3xl xl:object-[center_80%]
+            {/* Introduction section*/}
+            <div className="rounded-md dark:bg-black bg-white md:shadow-md">
+                {/* Image and description section */}
+                <div className="flex flex-col md:flex-row gap-4">
+                    <div>
+                        <Image
+                            src="/portfolio-img-2.jpg"
+                            alt="portfolio-img-2"
+                            width={0}
+                            height={0}
+                            className="w-full xl:w-3/5 h-72 object-cover object-bottom rounded-3xl xl:object-[center_80%]
                                     xl:translate-x-36"
-                        sizes="100vw"
-                    />
-                </div>
-                <div>
-                    <span className="text-black dark:text-white">{dict.QA.description}  <span className="font-bold text-(--text-color-primary-in-white) dark:text-(--text-color-in-dark)">{dict.QA.selenium}, {dict.QA.playwright}, {dict.QA.robot_framework}</span>.</span>
+                            sizes="100vw"
+                        />
+                    </div>
+                    <div>
+                        <span className="text-black dark:text-white">{dict.QA.description}  <span className="font-bold text-(--text-color-primary-in-white) dark:text-(--text-color-in-dark)">{dict.QA.selenium}, {dict.QA.playwright}, {dict.QA.robot_framework}</span>.</span>
 
-                    <div className="mt-5">
-                        <div className=" relative w-full rounded-xl border border-amber-500 dark:border-[#d3e97a] h-fit p-3 grid grid-cols-5 gap-2">
-                            <span className="text-black dark:text-[#c7c7c7] text-lg absolute left-4 -top-5 bg-white dark:bg-black p-2"> {dict.QA.tool}</span>
+                        <div className="mt-5">
+                            <div className=" relative w-full rounded-xl border border-amber-500 dark:border-[#d3e97a] h-fit p-3 grid grid-cols-5 gap-2">
+                                <span className="text-black dark:text-[#c7c7c7] text-lg absolute left-4 -top-5 bg-white dark:bg-black p-2"> {dict.QA.tool}</span>
 
-                            <div className="h-14 w-14 rounded-2xl p-2">
-                                <StackIcon name="selenium" />
-                            </div>
+                                <div className="h-14 w-14 rounded-2xl p-2">
+                                    <StackIcon name="selenium" />
+                                </div>
 
-                            <div className="h-14 w-14 rounded-2xl p-2">
-                                <StackIcon name="playwright" />
-                            </div>
+                                <div className="h-14 w-14 rounded-2xl p-2">
+                                    <StackIcon name="playwright" />
+                                </div>
 
-                            <div className="h-14 w-14 rounded-2xl p-2 flex items-center justify-center">
-                                <img className="dark:hidden" src={'/Robotframework--Streamline-Simple-Icons.png'} alt="Robotframework--Streamline-Simple-Icons" />
-                                <img className="hidden dark:block" src={'/Robotframework--Streamline-Simple-Icons-White.png'} alt="Robotframework--Streamline-Simple-Icons-White" />
+                                <div className="h-14 w-14 rounded-2xl p-2 flex items-center justify-center">
+                                    <img className="dark:hidden" src={'/Robotframework--Streamline-Simple-Icons.png'} alt="Robotframework--Streamline-Simple-Icons" />
+                                    <img className="hidden dark:block" src={'/Robotframework--Streamline-Simple-Icons-White.png'} alt="Robotframework--Streamline-Simple-Icons-White" />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
+                {/* Line break */}
                 <div className="border-b-2 border-b-(--color-paragraph) dark:border-b-(--border-color-in-dark) p-2 flex justify-center" />
 
-                <div className="dark:bg-[#171717] bg-[#f5f5f4] w-full p-2 text-black dark:text-white rounded-sm flex flex-col gap-2">
+                {/* Tools and technologies section */}
+                <div className="dark:bg-[#171717] bg-[#f5f5f4] w-full p-2 text-black dark:text-white rounded-sm flex flex-col gap-2 mt-5">
                     <div className="flex gap-2">
                         <IoMdCheckmark color="green" size={20} />
                         <p className=" truncate max-w-full">{dict.QA.list_checks.check_1}</p>
@@ -66,6 +73,44 @@ export default async function QA(props: paramProps) {
                         <IoMdCheckmark color="green" size={20} />
                         <p className=" truncate max-w-full">{dict.QA.list_checks.check_4}</p>
                     </div>
+                </div>
+
+            </div>
+
+            {/* Line break */}
+            <div className="border-b-2 border-b-(--color-paragraph) dark:border-b-(--border-color-in-dark) p-2 flex justify-center" />
+
+            {/* Manual section */}
+            <div>
+                <h2 className="text-black dark:text-white font-bold text-xl mb-3 text-center mt-3">{dict.QA.manual_section.introduction_title}</h2>
+                <p className="text-black dark:text-white text-center">{dict.QA.manual_section.description}</p>
+
+                {/* GG sheet */}
+                <div className="my-3">
+                    <iframe
+                        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRKxkb1_y_OM3uVDimOrXkjScJ9de8bykASHM0HRGwQh-z5SY8s8P46YZUYdba00c3McKSKnVFPs6hH/pubhtml?gid=0&single=true"
+                        width="100%"
+                        height="400">
+                    </iframe>
+                </div>
+            </div>
+
+            {/* Line break */}
+            <div className="border-b-2 border-b-(--color-paragraph) dark:border-b-(--border-color-in-dark) p-2 flex justify-center" />
+
+            {/* Automation section */}
+            <div>
+                <h2 className="text-black dark:text-white font-bold text-xl mb-3 text-center mt-3">{dict.QA.automation_section.introduction_title}</h2>
+                <p className="text-black dark:text-white text-center">{dict.QA.automation_section.description} <span className="text-blue-500 dark:text-(--text-color-in-dark)">GitHub Actions</span></p>
+
+                {/* Automation content */}
+                <div className="flex flex-col gap-3 mt-2">
+                    <div className="flex justify-center ">
+                        <div className="w-[70%] flex justify-center items-center gap-2 md:w-3/5 h-10 rounded-md bg-blue-600 hover:bg-blue-500 hover:cursor-pointer text-white">
+                            <FaPlay /> <span>Run Automation Tests</span>
+                        </div>
+                    </div>
+                    <div></div>
                 </div>
             </div>
         </div>
